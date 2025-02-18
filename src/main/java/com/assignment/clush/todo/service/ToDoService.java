13e572aff879f714942f97f0971ff26a5989298b
한결 <gyulbbe@gmail.com>
2025-02-18 09:24:47 +0900

    fix : 만료일과 생성일 비교

--- src/main/java/com/assignment/clush/todo/service/ToDoService.java
+++ src/main/java/com/assignment/clush/todo/service/ToDoService.java
@@ -1,5 +1,6 @@
 package com.assignment.clush.todo.service;
 
+import com.assignment.clush.common.DateCalculator;
 import com.assignment.clush.common.RestClushException;
 import com.assignment.clush.todo.dto.ConditionDto;
 import com.assignment.clush.todo.dto.ToDoByConditionDto;
@@ -30,6 +31,7 @@ public class ToDoService {
     }
 
     public ToDo insertToDo(ToDo toDo) {
+        compareDate(LocalDateTime.now(), toDo.getDueDate());
         toDo.setCreatedDate(LocalDateTime.now());
         toDoMapper.insertToDo(toDo);
         return toDoMapper.getToDoByNo(toDo.getNo());
@@ -37,7 +39,7 @@ public class ToDoService {
 
     public ToDo updateToDo(ToDo toDo) {
         isNotExisted(toDo.getNo());
-
+        compareDate(LocalDateTime.now(), toDo.getDueDate());
         ToDo prevToDo = getToDoByNo(toDo.getNo());
         if (prevToDo.equals(toDo)) {
             throw new RestClushException("수정된 부분이 존재하지 않습니다.");
@@ -61,6 +63,12 @@ public class ToDoService {
     private void isNotExisted(Integer toDoNo) {
         if (toDoMapper.countToDoByNo(toDoNo) == 0) {
             throw new RestClushException("존재하지 않는 할 일입니다.");
+        }
+    }
+
+    private void compareDate(LocalDateTime createdDate, LocalDateTime dueDate) {
+        if (!DateCalculator.dateCalculate(createdDate, dueDate)) {
+            throw new RestClushException("시작 날짜가 종료 날짜보다 이전이어야 합니다.");
         }
     }
 }
